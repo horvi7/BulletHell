f5c30eedf9c3bb36b307cbd4562d0bf3da452584
horvi7 <112907592+horvi7@users.noreply.github.com>
2023-11-05 20:15:27 +0100

    set1

---
+++ Tomegnyomor/Assets/ShootScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using Transform = UnityEngine.Transform;
+
+public class ShootScript : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform firingPoint;
+    [SerializeField] float bulletLifeTime = 3f;
+    [SerializeField] float shootingRate = 0.5f;
+    [SerializeField] float bulletSpeed = 0.5f;
+    private float fireTimer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireTimer += Time.deltaTime;
+        if(Input.GetMouseButton(0) && fireTimer >= shootingRate)
+        {
+            shoot();
+            fireTimer = 0;
+        }
+    }
+    private void shoot()
+    {
+        Vector3 shootDirection;
+        shootDirection = Input.mousePosition;
+        shootDirection.z = 0.0f;
+        shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+        shootDirection = shootDirection - firingPoint.position;
+        var bulletInstance = Instantiate(bulletPrefab, firingPoint.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        bulletInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(shootDirection.x * bulletSpeed, shootDirection.y * bulletSpeed);
+        Destroy(bulletInstance, bulletLifeTime);
+    }
+}
